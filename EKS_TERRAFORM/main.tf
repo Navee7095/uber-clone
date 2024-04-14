705edf615add45c8acc04166ecdcad8c468b62e3
Navee7095 <152251616+Navee7095@users.noreply.github.com>
2024-04-14 18:21:52 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -6,12 +6,10 @@ resource "aws_eks_cluster" "my_cluster" {
     subnet_ids         = ["subnet-0fac716e4e1c6c227", "subnet-0021745e2c2b844f2"]  # Replace with your subnet IDs
     security_group_ids = ["sg-0367c3d914d3c67d5"]                         # Replace with your security group IDs
   }
-
   tags = {
     Environment = "Production"
   }
 }
-
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
@@ -22,9 +20,8 @@ resource "aws_eks_node_group" "my_node_group" {
     desired_size = 2
     max_size     = 5
     min_size     = 1
-  }
-
-  tags = {
+}
+ tags = {
     Environment = "Production"
   }
 }
