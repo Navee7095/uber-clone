012e6e640eb9668582d975bad85f4359353b48de
Navee7095 <152251616+Navee7095@users.noreply.github.com>
2024-04-14 14:41:22 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "my_cluster" {
   role_arn = "arn:aws:iam::471112501164:role/admin_full_access"  # Replace with your IAM role ARN
 
   vpc_config {
-    subnet_ids         = ["subnet-0fac716e4e1c6c227", "subnet-093c04d40cd777b27"]  # Replace with your subnet IDs
+    subnet_ids         = ["subnet-0fac716e4e1c6c227", "subnet-0021745e2c2b844f2"]  # Replace with your subnet IDs
     security_group_ids = ["sg-0367c3d914d3c67d5"]                         # Replace with your security group IDs
   }
 
