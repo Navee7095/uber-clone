c11c79eafd3269e43153383f20d53e0359bbdc56
Navee7095 <152251616+Navee7095@users.noreply.github.com>
2024-04-14 14:53:20 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -16,7 +16,7 @@ resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
   node_role_arn   = "arn:aws:iam::471112501164:role/admin_full_access"  # Replace with your IAM role ARN
-  subnet_ids         = ["subnet-05258f73075fc3d05", "subnet-093c04d40cd777b27"]      # Replace with your subnet IDs
+  subnet_ids         = ["subnet-0fac716e4e1c6c227",", "subnet-0021745e2c2b844f2"]      # Replace with your subnet IDs
 
   scaling_config {
     desired_size = 2
