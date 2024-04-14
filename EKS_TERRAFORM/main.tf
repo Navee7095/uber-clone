778426825bc9a88832c7540a0494414592f3feb8
Navee7095 <152251616+Navee7095@users.noreply.github.com>
2024-04-14 19:34:44 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,15 +1,17 @@
 resource "aws_eks_cluster" "my_cluster" {
-  name     = "my-cluster"
+  name     = "my-cluster-1"
   role_arn = "arn:aws:iam::471112501164:role/admin_full_access"  # Replace with your IAM role ARN
 
   vpc_config {
     subnet_ids         = ["subnet-0fac716e4e1c6c227", "subnet-0021745e2c2b844f2"]  # Replace with your subnet IDs
     security_group_ids = ["sg-0367c3d914d3c67d5"]                         # Replace with your security group IDs
   }
+
   tags = {
     Environment = "Production"
   }
 }
+
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
