ef433d11de323731b05fc6ac4f397e83f9452c99
Navee7095 <152251616+Navee7095@users.noreply.github.com>
2024-04-15 16:09:52 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mys3bucketvcube" # Replace with your actual S3 bucket name
+    bucket = "naveen-vcube2" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-west-1"
   }
