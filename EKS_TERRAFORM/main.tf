e991cedc35c61e79d21f3245887f9434a3fc6fbd
Navee7095 <152251616+Navee7095@users.noreply.github.com>
2024-04-15 15:30:12 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,10 +1,10 @@
 resource "aws_eks_cluster" "my_cluster" {
-  name     = "my-cluster-1"
+  name     = "my-cluster-2"
   role_arn = "arn:aws:iam::471112501164:role/uber-access"  # Replace with your IAM role ARN
 
   vpc_config {
-    subnet_ids         = ["subnet-0fac716e4e1c6c227", "subnet-0021745e2c2b844f2"]  # Replace with your subnet IDs
-    security_group_ids = ["sg-0367c3d914d3c67d5"]                         # Replace with your security group IDs
+    subnet_ids         = ["subnet-036d61b2205dc884f", "subnet-010b3cbc50713a0da"]  # Replace with your subnet IDs
+    security_group_ids = ["sg-0659756effa32fa6d"]                         # Replace with your security group IDs
   }
 
   tags = {
@@ -16,7 +16,7 @@ resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
   node_role_arn   = "arn:aws:iam::471112501164:role/uber-access"  # Replace with your IAM role ARN
-  subnet_ids      = ["subnet-0fac716e4e1c6c227", "subnet-0021745e2c2b844f2"]      # Replace with your subnet IDs
+  subnet_ids      = ["subnet-036d61b2205dc884f", "subnet-010b3cbc50713a0da"]      # Replace with your subnet IDs
 
   scaling_config {
     desired_size = 2
